412e3c94fbac2c1fd0859ca895cbefba43c3b728
Harshit Raj <95363053+RajHarshit@users.noreply.github.com>
2024-12-02 19:32:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://realtime-database-c69da-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "https://add-to-cart-48114-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 
